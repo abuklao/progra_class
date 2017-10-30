3df12e5468182fb3d841819c6daaf58ffc77ac08
Ottersome <luisgarcia26284@gmail.com>
2017-10-31 04:12:51 +0800

    forgot to add comments :)

--- Week8/HW/0616115_hw6-1.c
+++ Week8/HW/0616115_hw6-1.c
@@ -6,6 +6,10 @@ int main(int argc,char *argv[]){
 	int total = 0;
 	char *ptr;
 	printf("This is your total\n");
+	//pretty simple
+	//we get the values form parameters
+	//we loop through the array
+	//and add them to the total
 	for(int i = 1;i< argc;i++){
 		total += strtol(argv[i],&ptr,10);
 	}
--- Week8/HW/0616115_hw6-2.c
+++ Week8/HW/0616115_hw6-2.c
@@ -5,15 +5,19 @@
 
 char points[][10] = {"AEILNORSTU","DG","BCMP","FHVWY","K","JX","QZ"};
 int main(void){
+	//basic variables for future use. we also ask for the user to enter his word
 	char str[MAX_STR];
 	int total_points = 0;
 	printf("Please enter your word\n");
+	//we will loop through the input and change charo to the respective character we loop through
 	char charo = '\0';	
 	//printf("letter e %c \n",points[0][1]);
-
+	//we loop until we find new line 
 	while((charo = getchar()) != '\n'){
+		//we change character to upper just in case
 		charo = toupper(charo);
-		printf("Letter : %c \n",charo);
+		//we loop through points and through every character and compare it to its position in the array
+		//once we determine this value(using the index) we add it to total_points
 		for(int i = 0; i < sizeof(points[0]);i++){
 			for(int j = 0;j < strlen(points[i]);j++){
 				if(charo == points[i][j]){
